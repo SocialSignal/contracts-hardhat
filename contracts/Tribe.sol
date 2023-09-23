73f1fb98ba732f60f6e40802de1085982240015f
João Alexandre Vaz Ferreira <30525975+joaoavf@users.noreply.github.com>
2023-09-23 06:01:20 +0000

    Added natspec

--- contracts/Tribe.sol
+++ contracts/Tribe.sol
@@ -4,75 +4,108 @@ pragma solidity ^0.8.19;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
+/// @title Tribe Membership Contract
+/// @dev A smart contract that mints ERC721 tokens as proof of membership within a tribe.
 contract Tribe is ERC721, Ownable {
-    mapping(address => bool) public memberAccepted;
-    mapping(address => bool) public ownerAccepted;
-    mapping(address => uint256) public tokenIdAddress;
-    uint256 public tokenCounter = 1; // tokenId = 0 means that it is empty
-    string private _nftImageURI;
-    string public ensName;
+    mapping(address => bool) public memberAccepted;  /// Tracks the acceptance status by the member
+    mapping(address => bool) public ownerAccepted;   /// Tracks the acceptance status by the owner
+    mapping(address => uint256) public tokenIdAddress;  /// Maps an address to its minted token ID
+    uint256 public tokenCounter = 1;  /// Keeps track of the last minted token ID. Set to 1 to enable token Id = 0, which means not minted.
+    string private _nftImageURI;  /// The base URI for all minted NFTs
+    string public ensName;  /// The ENS name associated with the contract
 
-    // Events
+    /// @notice Event emitted when a member's acceptance status changes
+    /// @param member The address of the member
+    /// @param value The new acceptance status
     event TribeMemberValue(address indexed member, bool value);
+
+    /// @notice Event emitted when an owner's acceptance status for a member changes
+    /// @param member The address of the member
+    /// @param value The new acceptance status
     event TribeOwnerValue(address indexed member, bool value);
+
+    /// @notice Event emitted when a new member joins the tribe
+    /// @param member The address of the member
+    /// @param tokenId The token ID that was minted
     event TribeJoined(address indexed member, uint256 tokenId);
+
+    /// @notice Event emitted when a member exits the tribe
+    /// @param member The address of the member
+    /// @param tokenId The token ID that was burned
     event TribeExited(address indexed member, uint256 tokenId);
 
+    /// @dev Only allows the owner or the sender themselves to perform certain actions
     modifier onlyOwnerOrSender(address member) {
         require(msg.sender == member || msg.sender == owner(), "msg.sender needs to be equal to member or owner");
         _;
     }
 
-    constructor(string memory name, string memory symbol, address owner, string memory nftImageURI, string memory ensName_) ERC721(name, symbol) {
+    /// @dev Initializes the contract
+    constructor(
+        string memory name, 
+        string memory symbol, 
+        address owner, 
+        string memory nftImageURI, 
+        string memory ensName_
+    ) ERC721(name, symbol) {
         transferOwnership(owner);
         _nftImageURI = nftImageURI;
         require(bytes(ensName_).length > 0, "ENS name cannot be empty");
         ensName = ensName_;
     }
 
+    /// @dev Overrides the `_transfer` function of the ERC721 standard
     function _transfer(
         address from,
         address to,
         uint256 tokenId
     ) internal virtual override {
-        // nothing happens on transfers
+        // disable transfers
     }
 
+    /// @dev Returns the base URI for the NFT image
     function _baseURI() internal view override returns (string memory) {
         return _nftImageURI;
-    }   
+    }
 
+    /// @dev Internal function to change the acceptance status of a member
     function _memberValue(address member, bool value) private {
         require(value != memberAccepted[member], "Value already set");
         memberAccepted[member] = value;
-        emit TribeMemberValue(member, value); // Emitting event
+        emit TribeMemberValue(member, value); 
     }
 
+    /// @dev Internal function to change the owner's acceptance status for a member
     function _ownerValue(address member, bool value) private {
         require(value != ownerAccepted[member], "Value already set");
         ownerAccepted[member] = value;
-        emit TribeOwnerValue(member, value); // Emitting event
+        emit TribeOwnerValue(member, value); 
     }
 
+    /// @dev Internal function to check if a member can be minted an NFT
     function _isMintable(address member) private view returns (bool) {
         return memberAccepted[member] && ownerAccepted[member];
     }
 
+    /// @dev Internal function to join a member to the tribe
     function _join(address member) private {
         uint256 nftId = tokenCounter;
         tokenIdAddress[member] = tokenCounter;
         tokenCounter = tokenCounter + 1;
         _safeMint(member, nftId);
-        emit TribeJoined(member, nftId); // Emitting event
+        emit TribeJoined(member, nftId); 
     }
 
+    /// @dev Internal function to exit a member from the tribe
     function _exit(address member) private {
         uint256 tokenId = tokenIdAddress[member];
         tokenIdAddress[member] = 0;
         _burn(tokenId);
-        emit TribeExited(member, tokenId); // Emitting event
+        emit TribeExited(member, tokenId); 
     }
 
+    /// @notice Accept a member into the tribe
+    /// @param member The address of the potential member
     function accept(address member) public onlyOwnerOrSender(member) {
         if (msg.sender == member) {
             _memberValue(member, true);
@@ -85,6 +118,8 @@ contract Tribe is ERC721, Ownable {
         }
     }
 
+    /// @notice Revoke a membership from the tribe
+    /// @param member The address of the current member
     function revoke(address member) public onlyOwnerOrSender(member) {
         if (msg.sender == member) {
             _memberValue(member, false);
@@ -96,11 +131,16 @@ contract Tribe is ERC721, Ownable {
             _exit(member);
         }
     }
-    
+
+    /// @notice Checks if an address is a member of the tribe
+    /// @param member The address to check
+    /// @return true if the address is a member, false otherwise
     function isMember(address member) public view returns (bool) {
         return balanceOf(member) > 0;
     }
 
+    /// @notice Sets the base URI for the NFT images
+    /// @param nftImageURI The new base URI
     function setBaseURI(string memory nftImageURI) external onlyOwner {
         _nftImageURI = nftImageURI;
     } 
